17bfd920b51b8c11f46d46e40146d611c316c018
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-12 14:30:35 +0100

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,7 +1,10 @@
 import pytest
-from src.exercise import read_records_from_file
+import os
 
 def test_exercise(capsys):
+    os.chdir('src')
+
+    from exercise import read_records_from_file
     file = "data.txt"
     records = read_records_from_file(file)
 
